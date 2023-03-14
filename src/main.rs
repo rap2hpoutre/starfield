5ad0f064bc79765f13a452889cf8969fefce82fc
John <john@meyerhoff.org>
2023-03-14 18:44:32 +0100

    Update to newest version of ggez
    The Library is now drawing differently,
    each star receives the canvas to be able to draw itself onto the canvas.

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,29 @@
 extern crate ggez;
 extern crate rand;
 
-use ggez::conf;
 use ggez::event::{self, EventHandler};
-use ggez::graphics::clear;
-use ggez::graphics::{present, Color, Rect};
-use ggez::timer::check_update_time;
+use ggez::graphics::{Color, Rect};
+use ggez::{conf, graphics};
 use ggez::{Context, ContextBuilder, GameResult};
 
 use rand::Rng;
 
-const WIDTH: f32 = 320.0;
-const HEIGHT: f32 = 200.0;
-const STARS_COUNT: u16 = 200;
-const XY_RANGE: i32 = 25;
-const MAX_DEPTH: u32 = 32;
+const WIDTH: f32 = 620.0;
+const HEIGHT: f32 = 500.0;
+const STARS_COUNT: u16 = 150;
+const XY_RANGE: i32 = 250;
+const MAX_DEPTH: u32 = 220;
 
 fn main() {
-    let ctx = &mut ContextBuilder::new("my_game", "Starfield")
+    let (ctx, event_loop) = ContextBuilder::new("my_game", "Starfield")
         .window_mode(conf::WindowMode::default().dimensions(WIDTH, HEIGHT))
         .build()
         .expect("aieee, could not create ggez context!");
 
-    let mut my_game = MyGame::new(&mut ctx.0);
+    let my_game = MyGame::new();
 
     // Run!
-    match event::run(&mut ctx.0, &mut ctx.1, &mut my_game) {
-        Ok(_) => println!("Exited cleanly."),
-        Err(e) => println!("Error occured: {}", e),
-    }
+    event::run(ctx, event_loop, my_game);
 }
 
 struct Star {
@@ -42,14 +37,35 @@ impl Star {
     fn new() -> Star {
         Star {
             x: xy_range(),
+
             y: xy_range(),
-            z: rand::thread_rng().gen_range(1, MAX_DEPTH) as f32,
+
+            z: rand::thread_rng().gen_range(50, MAX_DEPTH) as f32,
             color: MyGame::get_next_color(
                 //inclusive of low and exclusive of high ==> 0-255
                 rand::thread_rng().gen_range(0, 256) as u8,
                 rand::thread_rng().gen_range(0, 256) as u8,
                 rand::thread_rng().gen_range(0, 256) as u8,
             ),
+        }
+    }
+
+    fn draw(&self, canvas: &mut graphics::Canvas) -> () {
+        let half_width = WIDTH as f32 / 2.0;
+        let half_height = HEIGHT as f32 / 2.0;
+        let k = 128.0 / self.z;
+        let px: f32 = self.x * k + half_width;
+        let py: f32 = self.y * k + half_height;
+        if px >= 0.0 && px <= WIDTH as f32 && py >= 0.0 && py <= HEIGHT as f32 {
+            let size = 12.0;
+            let color = self.color;
+
+            canvas.draw(
+                &graphics::Quad,
+                graphics::DrawParam::default()
+                    .dest_rect(Rect::new(px, py, size, size))
+                    .color(color),
+            );
         }
     }
 }
@@ -59,22 +75,16 @@ fn xy_range() -> f32 {
 }
 
 struct MyGame {
-    half_width: f32,
-    half_height: f32,
     stars: Vec<Star>,
 }
 
 impl MyGame {
-    fn new(_ctx: &mut ggez::Context) -> MyGame {
+    fn new() -> Self {
         let mut stars: Vec<Star> = vec![];
         for _ in 0..STARS_COUNT {
             stars.push(Star::new());
         }
-        MyGame {
-            half_width: WIDTH as f32 / 2.0,
-            half_height: HEIGHT as f32 / 2.0,
-            stars,
-        }
+        MyGame { stars }
     }
 
     fn get_next_color(r: u8, g: u8, b: u8) -> Color {
@@ -82,11 +92,11 @@ impl MyGame {
     }
 }
 
-impl EventHandler for MyGame {
-    fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
-        while check_update_time(ctx, 30) {
+impl EventHandler<ggez::GameError> for MyGame {
+    fn update(&mut self, ctx: &mut Context) -> GameResult {
+        while ctx.time.check_update_time(120) {
             for star in &mut self.stars {
-                star.z -= 0.2;
+                star.z -= 0.3;
                 if star.z <= 0.0 {
                     star.x = xy_range();
                     star.y = xy_range();
@@ -97,26 +107,14 @@ impl EventHandler for MyGame {
 
         Ok(())
     }
-    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
-        clear(ctx, Color::from_rgb_u32(0x000000));
+    fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        // Color::from_rgb_u32(0x000000) clear
+        let mut canvas = graphics::Canvas::from_frame(ctx, Color::from_rgb_u32(0x010400));
         for star in &mut self.stars {
-            let k = 128.0 / star.z;
-            let px: f32 = star.x * k + self.half_width;
-            let py: f32 = star.y * k + self.half_height;
-            if px >= 0.0 && px <= WIDTH as f32 && py >= 0.0 && py <= HEIGHT as f32 {
-                let size = (1.0 - star.z / 32.0) * 5.0;
-                let color = star.color;
-                let rectangle = ggez::graphics::Mesh::new_rectangle(
-                    ctx,
-                    ggez::graphics::DrawMode::fill(),
-                    Rect::new(px, py, size, size),
-                    color,
-                )?;
-                ggez::graphics::draw(ctx, &rectangle, (ggez::mint::Point2 { x: 0.0, y: 0.0 },))
-                    .ok();
-            }
+            star.draw(&mut canvas);
         }
-        present(ctx).ok();
+        canvas.finish(ctx)?;
+        ggez::timer::yield_now();
         Ok(())
     }
 }
